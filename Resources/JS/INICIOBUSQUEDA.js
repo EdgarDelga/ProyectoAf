9f5c1a69325238d0cb67d0822be166bf017e2086
Edgar Delgadillo <95370665+EdgarDelga@users.noreply.github.com>
2022-08-19 12:07:26 -0500

    Add files via upload

---
+++ Resources/JS/INICIOBUSQUEDA.js
@@ -0,0 +1,8 @@
+function mostrar (){
+    document.getElementById("datos2").style.display = "flex";
+}
+
+function ocultar(){
+    document.getElementById("datos").style.display = "none";
+
+}
---
+++ Resources/REACT/formulario/src/components/formulario.jsx
@@ -0,0 +1,12 @@
+const Formulario = () => {
+    return (
+        <div clasName="form">
+            Este es mi Formulario
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default Formulario;
